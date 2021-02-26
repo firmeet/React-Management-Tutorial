9484a6f6dd50314789031b02b751c0735547bbfd
firmeet <firmeet@gmail.com>
2021-02-26 21:12:02 +0900

    Create Customer Information

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './Components/Customer'
+//import React, { Component } from 'react';
 
-function App() {
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/any',
+  'name': '방예랑',
+  'birthday': '800518',
+  'gender': '남자',
+  'job': '대학생'
+}, 
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/any',
+  'name': '김은주',
+  'birthday': '800518',
+  'gender': '남자',
+  'job': '대학생'
+}, 
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/any',
+  'name': '방민석',
+  'birthday': '800518',
+  'gender': '남자',
+  'job': '대학생'
+}
+]
+
+function App() {  
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>let's talk about kevin</h2>
+    <div>
+      {
+        customers.map(c=> {
+          return (
+          <Customer 
+          key={c.id}
+          id={c.id}
+          image={c.image}
+          name={c.name}
+          birthday={c.birthday}
+          gender={c.gender}
+          job={c.job}
+          />
+          )
+        })
+      }
     </div>
   );
 }
+
+// class App extends Component {
+//   render () {
+//     return (
+//     <Customer 
+//       name={customer.name}
+//       birthday={customer.birthday}
+//       gender={customer.gender}
+//       job={customer.job}
+//     />
+//     );
+//   }
+//}
 
 export default App;
